f6eab582a8fdd121dbed9422471ae24682c5b41e
SaaraAK <saara.alakorte1@gmail.com>
2019-11-30 14:12:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Voit lopettaa näppäimellä x ja painaa enteriä.");
+             int ryhmä, täydetHissit, viimeisessä;
+            string x;
+            do              
+       
+           {
+                Console.WriteLine("Anna ryhmän koko");
+                ryhmä = int.Parse(Console.ReadLine());
+        täydetHissit = ryhmä / 8; // kokonaisjako
+                viimeisessä = ryhmä % 8; // jakojäännöslaskutoimistus, paljonko jää kokonaisjaosta yli
+                Console.WriteLine("Täysiä hissejä {0}, viimiseen jää {1} henkilöä", täydetHissit, viimeisessä);
+                x = Console.ReadLine();
+                Console.WriteLine(x);
+            
+              
+            } while (x != "x");
+            
+       
+
+        }
+    }
+}
